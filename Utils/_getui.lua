535280f09076e13dcc47127b85e29128307e74e0
boydev1444 <cherm5754@gmail.com>
2024-07-26 17:45:02 -0600

    Update _getui.lua

--- Utils/_getui.lua
+++ Utils/_getui.lua
@@ -2,9 +2,14 @@
 local runService = game:GetService("RunService")
 
 --// main
-local IsStudio = runService:IsStudio()
-local IsServer = runService:IsServer()
-local IsClient = runService:IsClient()
+function _getui()
+	local IsStudio = runService:IsStudio()
+	local IsServer = runService:IsServer()
+	local IsClient = runService:IsClient()
 
-local CurrentGui = (IsStudio == true and game:GetService("StarterGui") or (IsClient == true and game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui") or game:GetService("StarterGui")))
-return CurrentGui
+	local CurrentGui = (IsStudio == true and game:GetService("StarterGui") or (IsClient == true and game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui") or game:GetService("StarterGui")))
+	return CurrentGui
+
+end
+
+return _getui
