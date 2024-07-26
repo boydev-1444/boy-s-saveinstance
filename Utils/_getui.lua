36ee98485efc91dd31c64c515f0c84af167d1262
boydev1444 <cherm5754@gmail.com>
2024-07-26 16:59:38 -0600

    Update _getui.lua

--- Utils/_getui.lua
+++ Utils/_getui.lua
@@ -1,1 +1,10 @@
+--// run service to detect the PlayerGui or StarterGui
+local runService = game:GetService("RunService")
 
+--// main
+local IsStudio = runService:IsStudio()
+local IsServer = runService:IsServer()
+local IsClient = runService:IsClient()
+
+local CurrentGui = (IsStudio == true and game:GetService("StarterGui") or (IsClient == true and game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui") or game:GetService("StarterGui")))
+return CurrentGui
